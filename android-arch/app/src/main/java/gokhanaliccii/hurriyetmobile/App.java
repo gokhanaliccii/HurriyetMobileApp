46184b3ed73bd8fba31bf8d856ed77915644ca9d
Gökhan Alıcı <gokhanaliccii@gmail.com>
2017-10-09 22:39:07 +0300

    Database initialiazed over DatabaseFacade

--- android-arch/app/src/main/java/gokhanaliccii/hurriyetmobile/App.java
+++ android-arch/app/src/main/java/gokhanaliccii/hurriyetmobile/App.java
@@ -2,9 +2,25 @@ package gokhanaliccii.hurriyetmobile;
 
 import android.app.Application;
 
+import gokhanaliccii.hurriyetmobile.db.AppDatabase;
+import gokhanaliccii.hurriyetmobile.db.facade.DatabaseFacade;
+
 /**
  * Created by gokhan on 08/10/17.
  */
 
 public class App extends Application {
+
+    AppDatabase database;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        init();
+    }
+
+    private void init() {
+        database = DatabaseFacade.newInstance(this).database();
+    }
 }
--- android-arch/app/src/main/java/gokhanaliccii/hurriyetmobile/db/facade/DatabaseFacade.java
+++ android-arch/app/src/main/java/gokhanaliccii/hurriyetmobile/db/facade/DatabaseFacade.java
@@ -1,11 +1,35 @@
 package gokhanaliccii.hurriyetmobile.db.facade;
 
+import android.app.Application;
+import android.arch.persistence.room.Room;
+
+import gokhanaliccii.hurriyetmobile.R;
+import gokhanaliccii.hurriyetmobile.db.AppDatabase;
+
 /**
  * Created by gokhan on 08/10/17.
  */
 
 public class DatabaseFacade {
 
+    private static DatabaseFacade instance;
 
+    private AppDatabase database;
 
+    private DatabaseFacade(Application app) {
+        final String dbName = app.getString(R.string.database_name);
+        database = Room.databaseBuilder(app, AppDatabase.class, dbName).build();
+    }
+
+    public static DatabaseFacade newInstance(Application app) {
+        if (instance == null) {
+            instance = new DatabaseFacade(app);
+        }
+
+        return instance;
+    }
+
+    public AppDatabase database() {
+        return database;
+    }
 }
