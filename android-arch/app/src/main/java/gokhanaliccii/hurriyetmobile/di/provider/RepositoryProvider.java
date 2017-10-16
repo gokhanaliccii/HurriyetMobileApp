eee267f9ad30ef4262d2839a7e972d3a8f4ba931
Gökhan Alıcı <gokhanaliccii@gmail.com>
2017-10-16 23:25:40 +0300

    Repository provider factory added to create viewmodel which has non empty constructor

---
+++ android-arch/app/src/main/java/gokhanaliccii/hurriyetmobile/di/provider/RepositoryProvider.java
@@ -0,0 +1,15 @@
+package gokhanaliccii.hurriyetmobile.di.provider;
+
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+
+/**
+ * Created by gokhan on 16/10/17.
+ */
+
+public class RepositoryProvider implements ViewModelProvider.Factory {
+    @Override
+    public <T extends ViewModel> T create(Class<T> modelClass) {
+        return null;
+    }
+}
---
+++ android-arch/app/src/main/java/gokhanaliccii/hurriyetmobile/view/writer/WriterViewModel.java
@@ -0,0 +1,16 @@
+package gokhanaliccii.hurriyetmobile.view.writer;
+
+import android.arch.lifecycle.ViewModel;
+
+import gokhanaliccii.hurriyetmobile.db.entity.WriterEntity;
+import gokhanaliccii.hurriyetmobile.persistence.core.Repository;
+
+/**
+ * Created by gokhan on 16/10/17.
+ */
+
+public class WriterViewModel extends ViewModel {
+
+    private Repository<WriterEntity> writerRepository;
+
+}
